39d99d1003a772edc14121bad396201f0c9fbb1b
damienroelens <roelens_dam@hotmail.com>
2014-01-31 17:34:01 +0100

    'final'

--- js/script.js
+++ js/script.js
@@ -13,24 +13,36 @@ function initialize(){
 	var textPanier,
 		pNode = document.createElement('p');
 	var divPanier = document.getElementsByClassName('panier')[0].children[0];
-	
-	if(JSON.parse(localStorage['donn']).length == 0){
+	var totpanier = 0;
+	if (localStorage['donn'] == undefined){
+		var dataExist = [];
+		localStorage['donn'] = [];
+	} else {
+		var dataExist = JSON.parse(localStorage['donn']);
+		for (var i = 0;i<dataExist.length; i++) {
+		 	totpanier = totpanier + parseFloat(dataExist[i]['prix']);
+		};
+		textPan = document.createTextNode('Total : '+totpanier.toFixed(2)+' €');
+		var ptot = document.createElement('p');
+		ptot.appendChild(textPan);
+	}
 
-		textPanier = document.createTextNode('0 articles dans le panier');
+	if(dataExist.length == 0){
+		textPanier = document.createTextNode('Pas d\'articles dans le panier');
 	} else {
 		textPanier = document.createTextNode(JSON.parse(localStorage['donn']).length+' article(s) dans le panier');
 	}
 
 	pNode.appendChild(textPanier);
 	divPanier.appendChild(pNode);
+	divPanier.appendChild(ptot);
 	//creation et comptage du panier
-
-	if(location.href == "file:///C:/Users/vdb040/Desktop/Etudes%20et%20vie/html/panier.html"){
-		
+	if(document.title == "Etudes et vie - Panier"){
 		if(localStorage['donn'].length>2){
 			var list = JSON.parse(localStorage['donn']);
+			var total = 0;
 			var container = document.createElement('ul');
-			document.getElementsByTagName('section')[0].appendChild(container);
+			document.getElementsByTagName('section')[0].children[0].appendChild(container);
 			for(var e=0;e<list.length;e++){
 
 				var delBut = document.createElement('a');
@@ -49,11 +61,23 @@ function initialize(){
 				item.appendChild(price);
 				item.appendChild(delBut);
 				container.appendChild(item);
+				total = total + parseFloat(list[e]['prix']);
 			}
+			
+			
+			var tot = document.createElement('h3');
+			var totspan = document.createElement('span');
+			tot.appendChild(document.createTextNode('Total'));
+			totspan.appendChild(document.createTextNode(total.toFixed(2)));
+			var litot = document.createElement('li');
+			litot.appendChild(tot);
+			litot.appendChild(totspan);
+			container.appendChild(litot);
+
 		} else {
 			var message = document.createElement('p');
 			message.appendChild(document.createTextNode('Vous n\'avez pas d\'article dans votre panier.'));
-			document.getElementsByTagName('section')[0].appendChild(message);
+			document.getElementsByTagName('section')[0].children[0].appendChild(message);
 		}
 	}
 
@@ -68,12 +92,11 @@ function addToCart(e){
 	if(localStorage['donn']){
 		oldItems = JSON.parse(localStorage['donn']);
 	}
-
-	var article = this.parentNode.children[0];
+	var article = this.parentNode;
 	var title = article.children[0].innerHTML;
-	var prix = article.children[3].innerHTML;
-	var description = article.children[2].innerHTML
-	var img = article.children[1].src;
+	var prix = article.children[1].children[1].innerHTML;
+	var description = article.children[1].children[2].innerHTML
+	var img = article.children[1].children[0].src;
 
 	for(var a=0;a<oldItems.length;a++){
 		if(oldItems[a]['title'] == title){
